ee847d5c9f4fee413965c802c0bfb45194330103
majidali129 <itsmajidali129@gmail.com>
2025-04-30 17:52:29 +0500

    added main tag

--- src/pages/Flights.tsx
+++ src/pages/Flights.tsx
@@ -2,7 +2,7 @@ import { CheapFlights, Destinations, Faqs, FlightsForm, Footer, Hero, PopularRou
 
 const FlightsPage = () => {
   return (
-    <>
+    <main>
       <Hero />
       <div className=" lg:max-w-5xl mx-auto  space-y-3 relative top-[109px] lg:top-[65.1px] lg:py-[29px] ">
         <FlightsForm />
@@ -14,7 +14,7 @@ const FlightsPage = () => {
         <Footer />
       </div>
       <div className=" h-[140px] lg:h-[65.1px] bg-background"></div>
-    </>
+    </main>
   );
 };
 export { FlightsPage };
